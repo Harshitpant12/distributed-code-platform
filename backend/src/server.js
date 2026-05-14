329ba9d66aaf4ffe73b1e88578208eabf2b0bb82
Harshitpant12 <harshpant778@gmail.com>
2026-05-14 20:44:34 +0530

    create setup to check socket connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,10 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
     console.log("User connected: ", socket.id);
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
+        console.log(`${socket.id} joined room ${roomId}`);
+    })
 
     socket.on("disconnect", ()=> {
         console.log("User disconnected:", socket.id);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import socket from './socket'
 
 function App() {
+
+  const joinRoom = () => {
+    socket.emit("join-room", "room123")
+  }
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected: ", socket.id);
+    })
+
+    return () => {
+      socket.off("connect");
+    }
+  }, [])
   return (
-    <div className='bg-amber-400'>App is live</div>
+    <div className='bg-amber-400'>
+      App is live
+      <button className='block bg-red-400 text-black' onClick={joinRoom}>
+        Join Room
+      </button>
+    </div>
   )
 }
 
